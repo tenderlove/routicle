cef1d13bed752a3d3b4f577c90eb852eb40ecb5c
Aaron Patterson <aaron.patterson@gmail.com>
2010-06-15 11:54:36 -0700

    grammar generation works!

--- lib/routicle/grammar_generator.rb
+++ lib/routicle/grammar_generator.rb
@@ -1,4 +1,6 @@
 require 'erb'
+require 'racc'
+require 'racc/grammarfileparser'
 
 module Routicle
   class GrammarGenerator
@@ -22,11 +24,33 @@ class GeneratedGrammar
 <% end %>
 rule<% @sentences.each do |sentence| %>
   url
-    : <%= sentence.first.join(' ') %> { <%= sentence.last.inspect %> }
+    : <%= sentence.first.join(' ') %> { result = <%= sentence.last.inspect %> }
   <% end %>
 end
+---- inner
+  def initialize scanner
+    @scanner = scanner
+  end
+
+  def parse str
+    @scanner.scan_setup str
+    do_parse
+  end
+
+  def next_token; @scanner.next_token; end
       eoruby
       template.result binding
+    end
+
+    ###
+    # Returns a Parser class that can be instantiated
+    def to_parser
+      parser     = Racc::GrammarFileParser.new
+      result     = parser.parse to_yacc
+      nfa        = Racc::States.new(result.grammar).nfa
+      parsegen   = Racc::ParserFileGenerator.new nfa.dfa, result.params
+      parser_src = parsegen.generate_parser
+      Module.new { class_eval parser_src }.const_get('GeneratedGrammar')
     end
   end
 end
--- test/test_grammar_generator.rb
+++ test/test_grammar_generator.rb
@@ -12,4 +12,11 @@ class TestGrammarGenerator < Test::Unit::TestCase
     assert_match(/ID/, grammar)
     assert_match(/rule/, grammar)
   end
+
+  def test_to_parser
+    gramgen = Routicle::GrammarGenerator.new
+    gramgen.add [:SLASH, :ID], { :foo => :bar }
+    parser = gramgen.to_parser.new Routicle::ScannerGenerator.new.compile
+    assert_equal({:foo => :bar}, parser.parse('/10'))
+  end
 end
